3175d5ee67d356f9b56f259cc23537e135c0ba18
Raden Fadhil A.A <fadhilanugrah21@upi.edu>
2023-11-15 20:58:43 +0700

    add file function

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,18 +1,26 @@
-import uploadFiles from '../utils/uploadFile.js';
+import asyncHandler from 'express-async-handler';
 
-export const testingUpload = async (
+import { generatePublicUrl, uploadFiles } from '../utils/uploadFile.js';
+
+export const testingUpload = asyncHandler(async (
   /** @type import('express').Request */ req,
   /** @type import('express').Response */ res,
 ) => {
   try {
     const resUpload = await uploadFiles('package.json');
+    const imageUrl = generatePublicUrl(resUpload[0].metadata.name);
     return res.status(200).json({
-      resUpload,
+      success: true,
+      message: `Image ${imageUrl} successfully uploaded`,
     });
   } catch (error) {
     console.log(error);
     return null;
   }
-};
+});
 
-export const testing = async () => {};
+export const updateImage = asyncHandler(async (
+  /** @type import('express').Request */ req,
+  /** @type import('express').Response */ res,
+) => {  
+});
--- src/utils/uploadFile.js
+++ src/utils/uploadFile.js
@@ -1,14 +1,14 @@
-import { Storage } from "@google-cloud/storage";
-import * as dotenv from "dotenv";
+import { Storage } from '@google-cloud/storage';
+import * as dotenv from 'dotenv';
 
 dotenv.config();
-export default async function uploadFiles(file) {
+const storage = new Storage({
+  projectId: process.env.PROJECT_ID,
+  keyFilename: process.env.KEY_SERVICE_ACCOUNT,
+});
+const bucket = storage.bucket(process.env.BUCKET_NAME);
+export async function uploadFiles(file) {
   try {
-    const storage = new Storage({
-      projectId: process.env.PROJECT_ID,
-      keyFilename: process.env.KEY_SERVICE_ACCOUNT,
-    });
-    const bucket = storage.bucket(process.env.BUCKET_NAME);
     const upload = await bucket.upload(file);
 
     return upload;
@@ -16,4 +16,13 @@ export default async function uploadFiles(file) {
     console.log(error);
     return null;
   }
+}
+
+export function generatePublicUrl(objectName) {
+  return `https://storage.googleapis.com/${process.env.BUCKET_NAME}/${objectName}`;
+}
+
+export async function deleteFiles(fileName) {
+  const file = bucket.file(fileName);
+  await file.delete();
 }
