ceecffd31e7f1d4c8c81738a47f69f78b0bdcf85
Fadhil <fadhilanugrah21@gmail.com>
2023-12-03 11:07:27 +0700

    add some progress

--- src/controllers/rekomendasi.controller.js
+++ src/controllers/rekomendasi.controller.js
@@ -1,7 +1,12 @@
 import asyncHandler from 'express-async-handler';
+import axios from 'axios';
+import * as dotenv from 'dotenv';
+
 import rekomendasi from '../models/rekomendasi.model.js';
 import { errorResponse } from '../utils/response.js';
 import { getCoordinates, getKabKot, getProvinsi } from '../utils/lokasi.js';
+import { getAQI, getWeather } from '../utils/getAPI.js';
+
 
 export const createRekomendasi = asyncHandler(
   async (
@@ -10,23 +15,24 @@ export const createRekomendasi = asyncHandler(
   ) => {
     try {
       const data = await getKabKot(11);
-      await getCoordinates('KABUPATEN ACEH BARAT');
-      res.status(200).json(data);
+      const coordinates = await getCoordinates('KABUPATEN ACEH BARAT');
+      const aqi = await getAQI(coordinates.lng, coordinates.lat);
+      const weather = await getWeather(coordinates.lng, coordinates.lat);
+      res.status(200).json({ coordinates, weather: weather.data, aqi });
     } catch (error) {
       errorResponse(res, error.message, 500);
     }
   },
 );
 
-// export const getAqi = asyncHandler(
-//   async (
-//     /** @type import('express').Request */ req,
-//     /** @type import('express').Response */ res,
-//   ) => {
-//     try {
-
-//     } catch (error) {
-//       errorResponse(res, error.message, 500);
-//     }
-//   },
-// );
+export const getAqi = asyncHandler(
+  async (
+    /** @type import('express').Request */ req,
+    /** @type import('express').Response */ res,
+  ) => {
+    try {
+    } catch (error) {
+      errorResponse(res, error.message, 500);
+    }
+  },
+);
