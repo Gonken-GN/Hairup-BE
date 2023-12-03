1d4a5e4392166af63ec9e41ba38cd01175815121
Fadhil <fadhilanugrah21@gmail.com>
2023-12-04 01:18:56 +0700

    Add notifikasi route and controller for server-sent events

---
+++ src/controllers/notifikasi.controller.js
@@ -0,0 +1,27 @@
+import asyncHandler from 'express-async-handler';
+import { addClient, removeClient, sendToAllClients } from '../utils/clientManager.js';
+
+export const sseController = {
+  connect: asyncHandler((
+    /** @type import('express').Request */ req,
+    /** @type import('express').Response */ res,
+  ) => {
+    res.setHeader('Content-Type', 'text/event-stream');
+    res.setHeader('Cache-Control', 'no-cache');
+    res.setHeader('Connection', 'keep-alive');
+    res.flushHeaders();
+
+    const clientId = Date.now();
+    const newClient = { id: clientId, res };
+    addClient(newClient);
+    // Function to send updates
+    setInterval(() => {
+      const message = { timestamp: new Date(), message: 'Hourly update' };
+  const formattedMessage = `data: ${JSON.stringify(message)}\n\n`;
+  sendToAllClients(formattedMessage);
+    }, 10000);
+    req.on('close', () => {
+      removeClient(clientId);
+    });
+  }),
+};
--- src/utils/clientManager.js
+++ src/utils/clientManager.js
@@ -1,22 +1,18 @@
 // clientManager.js
 let clients = [];
 
-const addClient = (client) => {
+export const addClient = (client) => {
   clients.push(client);
 };
 
-const removeClient = (id) => {
+export const removeClient = (id) => {
   clients = clients.filter((client) => client.id !== id);
 };
 
-const sendToAllClients = (data) => {
+export const sendToAllClients = (data) => {
   clients.forEach((client) => client.res.write(data));
 };
 
-// Function to send updates
-setInterval(() => {
-  const message = { timestamp: new Date(), message: 'Hourly update' };
-  sendToAllClients(message);
-}, 3600000); // 1 hour
+ // 1 hour
 
-module.exports = { addClient, removeClient, sendToAllClients };
+// module.exports = { addClient, removeClient, sendToAllClients };
