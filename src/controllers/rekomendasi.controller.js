c8a0f8024cac70b21668f937d8b620977f2eb6df
Fadhil <fadhilanugrah21@gmail.com>
2023-12-04 21:29:20 +0700

    fix error

--- src/controllers/rekomendasi.controller.js
+++ src/controllers/rekomendasi.controller.js
@@ -7,7 +7,6 @@ import { errorResponse } from '../utils/response.js';
 import { getCoordinates, getKabKot, getProvinsi } from '../utils/lokasi.js';
 import { getAQI, getWeather } from '../utils/getAPI.js';
 
-
 export const createRekomendasi = asyncHandler(
   async (
     /** @type import('express').Request */ req,
@@ -41,31 +40,30 @@ export const createRekomendasi1 = asyncHandler(
   async (
     /** @type import('express').Request */ req,
     /** @type import('express').Response */ res,
-  ) =>{
+  ) => {
     try {
       const age = await getUmur();
     } catch (error) {
       errorResponse(res, error.message, 500);
     }
-  }
+  },
 );
 
 export const createRekomendasi2 = asyncHandler(
   async (
     /** @type import('express').Request */ req,
     /** @type import('express').Response */ res,
-  ) =>{
+  ) => {
     try {
-      const {status, riwayatPenyakit} = req.body
-      if (statusAnswer === 'pregnant' || statusAnswer === 'athletes'){
+      const { status, riwayatPenyakit } = req.body;
+      if (status === 'pregnant' || status === 'athletes') {
         res.status(200).json(status);
-      }else if (
-        (riwayatPenyakitAnswer === 'Heart Disease' || riwayatPenyakitAnswer === 'Lung Disease'){
-          res.status(200).json(riwayatPenyakit);
-        });
-      
+      } else if (
+        riwayatPenyakit === 'Heart Disease' || riwayatPenyakit === 'Lung Disease') {
+        res.status(200).json(riwayatPenyakit);
+      }
     } catch (error) {
       errorResponse(res, error.message, 500);
     }
-  }
-);1
+  },
+); 1;
