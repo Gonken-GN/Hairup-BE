ad5e6a7e6e49facd00206780735132ad175c1c03
Fauzan Pratama <fauzanpra52@gmail.com>
2023-12-05 21:00:23 +0700

    Rekomendasi Crud

--- src/controllers/rekomendasi.controller.js
+++ src/controllers/rekomendasi.controller.js
@@ -7,54 +7,124 @@ import { errorResponse } from '../utils/response.js';
 import { getCoordinates, getKabKot, getProvinsi } from '../utils/lokasi.js';
 import { getAQI, getWeather } from '../utils/getAPI.js';
 
+// export const createRekomendasi = asyncHandler(
+//   async (
+//     /** @type import('express').Request */ req,
+//     /** @type import('express').Response */ res,
+//   ) => {
+//     try {
+//       // const data = await getKabKot(11);
+//       const coordinates = await getCoordinates('KABUPATEN ACEH BARAT');
+//       const aqi = await getAQI(coordinates.lng, coordinates.lat);
+//       const weather = await getWeather(coordinates.lng, coordinates.lat);
+//       res.status(200).json({ coordinates, weather: weather.data, aqi });
+//     } catch (error) {
+//       errorResponse(res, error.message, 500);
+//     }
+//   },
+// );
+
+// export const getAqi = asyncHandler(
+//   async (
+//     /** @type import('express').Request */ req,
+//     /** @type import('express').Response */ res,
+//   ) => {
+//     try {
+//     } catch (error) {
+//       errorResponse(res, error.message, 500);
+//     }
+//   },
+// );
+
+export const getRekomendasi = asyncHandler(
+  async (
+    /** @type import('express').Request */ req,
+    /** @type import('express').Response */ res,
+  ) => {
+    try {
+      const response = await Rekomendasi.findAll();
+      res.status(200).json(response);
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        message: error.message,
+      });
+    }
+  },
+);
+
+export const getRekomendasiById = asyncHandler(
+  async (
+    /** @type import('express').Request */ req,
+    /** @type import('express').Response */ res,
+  ) => {
+    try {
+      const user = await Rekomendasi.findOne({
+        where: {
+          id: req.params.id,
+        },
+      });
+      res.status(200).json({
+        success: true,
+        user,
+      });
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        message: error.message,
+      });
+    }
+  },
+);
+
+// export const createRekomendasi2 = asyncHandler(
+//   async (
+//     /** @type import('express').Request */ req,
+//     /** @type import('express').Response */ res,
+//   ) => {
+//     try {
+//       if (req.body.files) {
+//         const umur = await Umur.create('package.json');
+//         const user = await User.create({ ...req.body, avatarUrl });
+//         res.status(200).json({
+//           success: true,
+//           user,
+//         });
+//       }
+//       const user = await User.create(req.body);
+//       res.status(200).json({
+//         success: true,
+//         user,
+//       });
+//     } catch (error) {
+//       res.status(500).json({
+//         success: false,
+//         message: error.message,
+//       });
+//     }
+//   },
+// );
+
+// export const createRekomendasi1 = asyncHandler(
+//   async (
+//     /** @type import('express').Request */ req,
+//     /** @type import('express').Response */ res,
+//   ) => {
+//     try {
+//       const age = await getUmur();
+//     } catch (error) {
+//       errorResponse(res, error.message, 500);
+//     }
+//   },
+// );
+
 export const createRekomendasi = asyncHandler(
   async (
     /** @type import('express').Request */ req,
     /** @type import('express').Response */ res,
   ) => {
     try {
-      // const data = await getKabKot(11);
-      const coordinates = await getCoordinates('KABUPATEN ACEH BARAT');
-      const aqi = await getAQI(coordinates.lng, coordinates.lat);
-      const weather = await getWeather(coordinates.lng, coordinates.lat);
-      res.status(200).json({ coordinates, weather: weather.data, aqi });
-    } catch (error) {
-      errorResponse(res, error.message, 500);
-    }
-  },
-);
-
-export const getAqi = asyncHandler(
-  async (
-    /** @type import('express').Request */ req,
-    /** @type import('express').Response */ res,
-  ) => {
-    try {
-    } catch (error) {
-      errorResponse(res, error.message, 500);
-    }
-  },
-);
-
-export const createRekomendasi1 = asyncHandler(
-  async (
-    /** @type import('express').Request */ req,
-    /** @type import('express').Response */ res,
-  ) => {
-    try {
-      const age = await getUmur();
-    } catch (error) {
-      errorResponse(res, error.message, 500);
-    }
-  },
-);
-
-export const createRekomendasi2 = asyncHandler(
-  async (
-    /** @type import('express').Request */ req,
-    /** @type import('express').Response */ res,
-  ) => {
-    try {
+      const { umur } = req.body;
       const { status, riwayatPenyakit } = req.body;
       if (status === 'pregnant' || status === 'athletes') {
         res.status(200).json(status);
@@ -67,3 +137,29 @@ export const createRekomendasi2 = asyncHandler(
     }
   },
 ); 1;
+
+export const deleteRekomendasi = asyncHandler(
+  async (
+    /** @type import('express').Request */ req,
+    /** @type import('express').Response */ res,
+  ) => {
+    try {
+      const user = await Rekomendasi.findOne({
+        where: {
+          id: req.params.id,
+        },
+      });
+      await Rekomendasi.destroy({
+        where: {
+          id: req.params.id,
+        },
+      });
+      res.status(200).json({ message: 'Rekomendasi Deleted' });
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        message: error.message,
+      });
+    }
+  },
+);
